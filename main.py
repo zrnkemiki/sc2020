c8628726990571166755bc8d3a7afa9577c72042
zrnkemiki <zrnkemiki@gmail.com>
2021-02-10 17:01:40 +0100

    img eroded

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
+
 def load_res():
     number_of_people_res = []
     with open('data/res.txt') as file:
@@ -13,31 +14,21 @@ def load_res():
             number_of_people_res.append(people)
     return number_of_people_res
 
+
 def write_data(my_data):
     file = open('out.txt', "w")
-    file.write("SW93-2017/Milos Zrnic")
     file.write("file,count")
     file.write("\n")
-    for i in range (0,len(my_data)):
-        data = "video" + str(i+1) + ".mp4," + str(my_data[i])
+    for i in range(0, len(my_data)):
+        data = "video" + str(i + 1) + ".mp4," + str(my_data[i])
         file.write(data)
         file.write("\n")
+    file.write("ZRNIC MILOS SW93-2017")
     file.close()
 
 
 def adaptive_threshold(image):
     return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, 4)
-
-# def dilate(image, iterations, kernel=kernel_size):
-#    return cv2.dilate(image, kernel, iterations=iterations)
-
-
-# def erode(image, iterations, kernel=kernel_size):
-#    return cv2.erode(image, kernel, iterations=iterations)
-
-
-# def closing(image, dilate_iterations, erode_iterations, dilate_kernel=kernel_size, erode_kernel=kernel_size):
-#    return erode(dilate(image, dilate_iterations, dilate_kernel), erode_iterations, erode_kernel)
 
 
 def plato_line(firstVideo):
@@ -53,7 +44,7 @@ def plato_line(firstVideo):
         ret_val, frame1 = capture.read()
 
     # Bitno nam je samo prvi frejm da iskoristimo za fiksiranje platoa zato sto kameru ne pomeramo
-    frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
+    frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)    
 
     low_threshold = 170
     high_threshold = 250
@@ -80,8 +71,15 @@ def plato_line(firstVideo):
 
 def pedestrian_cross_detection(y, yy):
     if abs(yy - y) < 7.5:
-        print("Pedestrian detected")
+        #print("Pedestrian detected")
         return True
+
+
+def opening(image):
+    k = np.ones((3, 3))
+    dilated = cv2.dilate(image, k, iterations=2)
+    eroded = cv2.erode(dilated, k, iterations=2)
+    return eroded
 
 
 def count_people(video_path, y1, y2):
@@ -90,7 +88,7 @@ def count_people(video_path, y1, y2):
     # crossing line y cordinate
     line_y = (y1 + y2) / 2
     # Neuspeli pokusaj da brojim izmedju dve linije pesake
-    #line_y2 = (y1 + y2 + 35) / 2
+    # line_y2 = (y1 + y2 + 35) / 2
 
     cap = cv2.VideoCapture(video_path)
     _, frame = cap.read()
@@ -105,17 +103,15 @@ def count_people(video_path, y1, y2):
 
         counter += 1
 
-        #Svaki 5frejm daje mi dobre rez
+        # Svaki 5frejm daje mi dobre rez
         if counter != 5:
             continue
         counter = 0
 
-        # current_frame = imutils.resize(current_frame, width=500)
         current_gray = cv2.cvtColor(current_frame, cv2.COLOR_RGB2GRAY)
         # smoothing to remove noise, ne daje bolji rez
         # current_frame_gray = cv2.GaussianBlur(current_frame_gray, (21, 21), 0)
 
-        # ext_frame = imutils.resize(next_frame, width=500)
         next_gray = cv2.cvtColor(next_frame, cv2.COLOR_RGB2GRAY)
         # smoothing to remove noise, ne daje bolji rez
         # next_frame_gray = cv2.GaussianBlur(next_frame_gray, (21, 21), 0)
@@ -136,21 +132,29 @@ def count_people(video_path, y1, y2):
         # cv2.imshow("Threshold", threshold)
         # cv2.waitKey(5)
 
-        threshold = cv2.dilate(threshold, None, iterations=2)
-        #cv2.imshow("Threshold", threshold)
-        #cv2.waitKey(5)
-        contours, _ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        threshold = opening(threshold)
+        #threshold = cv2.dilate(threshold, None, iterations=2)
+        #threshold = cv2.erode(threshold, k, iterations=2)
+        # Sa None vrednosti kernela, MAE 2.7
+        # threshold = cv2.erode(threshold, None, iterations=2)
 
+        # cv2.imshow("Threshold", threshold)
+        # cv2.waitKey(5)
+        contours, _ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        mostValues = []
+        mostValuesH = []
         for contour in contours:
             rect = cv2.minAreaRect(contour)
             (x, y), (width, height), _ = rect
-            box = cv2.boxPoints(rect)
-            box = np.int0(box)
+            # box = cv2.boxPoints(rect)
+            # box = np.int0(box)
             # cv2.drawContours(cop, [box], 0, (0, 255, 0), 2)
             # cv2.imshow("resultimage", cop)
             # cv2.waitKey(1)
-            if 10 < width < 70 or 10 < height < 70:
-            #if cv2.contourArea(contour) > 150:
+            mostValues.append(width)
+            mostValuesH.append(height)
+            if 10 < width < 100 or 12 < height < 100:
+                # if cv2.contourArea(contour) > 130: IPAK NE
                 if pedestrian_cross_detection(y, line_y):
                     number_of_people += 1
     cap.release()
